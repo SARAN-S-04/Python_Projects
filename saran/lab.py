fef5630fe710dd393f00c418bb7c2f0d4598ff8f
SARAN-S-04 <123234859+SARAN-S-04@users.noreply.github.com>
2024-06-25 11:06:31 +0530

    Delete saran directory

--- saran/lab.py
+++
@@ -1,9 +0,0 @@
-original_grades = [56, 99, 67, 54, 91, 90, 85, 79, 97, 66, 68, 94]
-
-# Sort the grades
-sorted_grades = sorted(original_grades)
-
-# Display the sorted grades
-print("Sorted Student Grades:")
-for grade in sorted_grades:
-    print(grade)
